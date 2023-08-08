e361bc347913988e0ad980ad699228fac5291ba7
Rohan  Ilyas <rohanilyas@aussourceenterprises.com>
2023-08-08 23:45:44 +0500

    Auth0 implementation

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain="dev-2wqnljb42wm16l1q.us.auth0.com"
+    clientId="8kQdvdrHuq1Jw9sEelrIloARwhFGovaU"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+      audience: "https://dev-2wqnljb42wm16l1q.us.auth0.com/api/v2/",
+      scope: "read:current_user update:current_user_metadata"
+    }}
+  >
     <App />
-  </React.StrictMode>
+  </Auth0Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
